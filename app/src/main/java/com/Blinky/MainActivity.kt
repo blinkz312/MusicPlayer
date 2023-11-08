357bd034f9767fe08ef67807ccae5b6eaf2ef773
blinkz312 <chirayurana@yahoo.com>
2023-11-08 11:17:23 +0545

    Stop watch app changed to music player

--- app/src/main/java/com/Blinky/MainActivity.kt
+++ app/src/main/java/com/Blinky/MainActivity.kt
@@ -2,10 +2,21 @@ package com.Blinky
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.sql.Time
+import java.util.Locale
+import java.util.Timer
 
 class MainActivity : AppCompatActivity() {
+    private var seconds = 0
+    private var running = false
+    private var wasRunning = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        }
     }
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "StopwatchApp"
+rootProject.name = "MusicPlayer"
 include(":app")
