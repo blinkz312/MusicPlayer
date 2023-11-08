f71b85ed3b856aa5097d57940a3a92d316522700
blinkz312 <chirayurana@yahoo.com>
2023-11-08 14:32:09 +0545

    Simple music player/No unique features

--- app/src/main/java/com/Blinky/MainActivity.kt
+++ app/src/main/java/com/Blinky/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.Blinky
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.SystemClock
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import java.sql.Time
-import java.util.Locale
-import java.util.Timer
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    private var seconds = 0
-    private var running = false
-    private var wasRunning = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val play = findViewById<Button>(R.id.startBtn)
+        val pause = findViewById<Button>(R.id.pauseBtn)
+        val image = findViewById<ImageView>(R.id.img)
+        //Creating a mediaplayer object
+        var mediaPlayer = MediaPlayer.create(this,R.raw.music1)
+        //Creating onClickListener object to play
+        play.setOnClickListener {
+                image.setImageResource(R.drawable.baseline_play_circle_24)
+                mediaPlayer.start()
+        }
+        //Creating onCLickListener object to pause
+        pause.setOnClickListener {
+            image.setImageResource(R.drawable.baseline_pause_circle_outline_24)
+            mediaPlayer.pause()
+        }
         }
     }
